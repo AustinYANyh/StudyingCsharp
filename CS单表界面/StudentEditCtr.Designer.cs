a068ec571f69431d4f6e22c7d46e626c40e6a8ec
AustinYANyh <1241528539@qq.com>
2019-11-21 21:28:34 +0800

    弹出界面的代码（组件设计器生成）

---
+++ CS单表界面/StudentEditCtr.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ORAPS.Fabric
+{
+    partial class StudentEditCtr
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RGB_AboutMateria = new Telerik.WinControls.UI.RadGroupBox();
+            this.DTP_ReleaseDate = new Telerik.WinControls.UI.RadDateTimePicker();
+            this.DTP_DueDate = new Telerik.WinControls.UI.RadDateTimePicker();
+            this.LAB_ReleaseDate = new Telerik.WinControls.UI.RadLabel();
+            this.LAB_ComputedReleaseDate = new Telerik.WinControls.UI.RadLabel();
+            this.LAB_ID = new Telerik.WinControls.UI.RadLabel();
+            this.LAB_ComputedDueDate = new Telerik.WinControls.UI.RadLabel();
+            this.LAB_OrgDueDate = new Telerik.WinControls.UI.RadLabel();
+            this.TXT_Name = new Telerik.WinControls.UI.RadTextBox();
+            this.LAB_DueDate = new Telerik.WinControls.UI.RadLabel();
+            this.TXT_ID = new Telerik.WinControls.UI.RadTextBox();
+            this.LAB_Name = new Telerik.WinControls.UI.RadLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.BTN_OK)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BTN_Cancel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RGB_AboutMateria)).BeginInit();
+            this.RGB_AboutMateria.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DTP_ReleaseDate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DTP_DueDate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_ReleaseDate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_ComputedReleaseDate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_ID)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_ComputedDueDate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_OrgDueDate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TXT_Name)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_DueDate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TXT_ID)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_Name)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BTN_OK
+            // 
+            this.BTN_OK.Location = new System.Drawing.Point(194, 280);
+            // 
+            // BTN_Cancel
+            // 
+            this.BTN_Cancel.Location = new System.Drawing.Point(273, 280);
+            // 
+            // RGB_AboutMateria
+            // 
+            this.RGB_AboutMateria.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.RGB_AboutMateria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(234)))), ((int)(((byte)(240)))));
+            this.RGB_AboutMateria.Controls.Add(this.DTP_ReleaseDate);
+            this.RGB_AboutMateria.Controls.Add(this.DTP_DueDate);
+            this.RGB_AboutMateria.Controls.Add(this.LAB_ReleaseDate);
+            this.RGB_AboutMateria.Controls.Add(this.LAB_ComputedReleaseDate);
+            this.RGB_AboutMateria.Controls.Add(this.LAB_ID);
+            this.RGB_AboutMateria.Controls.Add(this.LAB_ComputedDueDate);
+            this.RGB_AboutMateria.Controls.Add(this.LAB_OrgDueDate);
+            this.RGB_AboutMateria.Controls.Add(this.TXT_Name);
+            this.RGB_AboutMateria.Controls.Add(this.LAB_DueDate);
+            this.RGB_AboutMateria.Controls.Add(this.TXT_ID);
+            this.RGB_AboutMateria.Controls.Add(this.LAB_Name);
+            this.RGB_AboutMateria.HeaderText = "AboutMateria";
+            this.RGB_AboutMateria.Location = new System.Drawing.Point(19, 15);
+            this.RGB_AboutMateria.Name = "RGB_AboutMateria";
+            this.RGB_AboutMateria.Size = new System.Drawing.Size(327, 247);
+            this.RGB_AboutMateria.TabIndex = 34;
+            this.RGB_AboutMateria.Text = "AboutMateria";
+            // 
+            // DTP_ReleaseDate
+            // 
+            this.DTP_ReleaseDate.Location = new System.Drawing.Point(153, 461);
+            this.DTP_ReleaseDate.Name = "DTP_ReleaseDate";
+            this.DTP_ReleaseDate.Size = new System.Drawing.Size(110, 20);
+            this.DTP_ReleaseDate.TabIndex = 31;
+            this.DTP_ReleaseDate.TabStop = false;
+            this.DTP_ReleaseDate.Tag = "ReleaseDate";
+            this.DTP_ReleaseDate.Text = "2016年7月11日";
+            this.DTP_ReleaseDate.Value = new System.DateTime(2016, 7, 11, 16, 1, 46, 140);
+            this.DTP_ReleaseDate.Visible = false;
+            // 
+            // DTP_DueDate
+            // 
+            this.DTP_DueDate.Location = new System.Drawing.Point(153, 565);
+            this.DTP_DueDate.Name = "DTP_DueDate";
+            this.DTP_DueDate.Size = new System.Drawing.Size(110, 20);
+            this.DTP_DueDate.TabIndex = 35;
+            this.DTP_DueDate.TabStop = false;
+            this.DTP_DueDate.Tag = "DueDate";
+            this.DTP_DueDate.Text = "2016年7月11日";
+            this.DTP_DueDate.Value = new System.DateTime(2016, 7, 11, 16, 1, 46, 140);
+            this.DTP_DueDate.Visible = false;
+            // 
+            // LAB_ReleaseDate
+            // 
+            this.LAB_ReleaseDate.Location = new System.Drawing.Point(20, 462);
+            this.LAB_ReleaseDate.Name = "LAB_ReleaseDate";
+            this.LAB_ReleaseDate.Size = new System.Drawing.Size(68, 18);
+            this.LAB_ReleaseDate.TabIndex = 5;
+            this.LAB_ReleaseDate.Tag = "ReleaseDate";
+            this.LAB_ReleaseDate.Text = "ReleaseDate";
+            this.LAB_ReleaseDate.Visible = false;
+            // 
+            // LAB_ComputedReleaseDate
+            // 
+            this.LAB_ComputedReleaseDate.Location = new System.Drawing.Point(20, 488);
+            this.LAB_ComputedReleaseDate.Name = "LAB_ComputedReleaseDate";
+            this.LAB_ComputedReleaseDate.Size = new System.Drawing.Size(120, 18);
+            this.LAB_ComputedReleaseDate.TabIndex = 6;
+            this.LAB_ComputedReleaseDate.Tag = "ComputedReleaseDate";
+            this.LAB_ComputedReleaseDate.Text = "ComputedReleaseDate";
+            this.LAB_ComputedReleaseDate.Visible = false;
+            // 
+            // LAB_ID
+            // 
+            this.LAB_ID.Location = new System.Drawing.Point(39, 53);
+            this.LAB_ID.Name = "LAB_ID";
+            this.LAB_ID.Size = new System.Drawing.Size(17, 18);
+            this.LAB_ID.TabIndex = 3;
+            this.LAB_ID.Tag = "ID";
+            this.LAB_ID.Text = "ID";
+            // 
+            // LAB_ComputedDueDate
+            // 
+            this.LAB_ComputedDueDate.Location = new System.Drawing.Point(20, 540);
+            this.LAB_ComputedDueDate.Name = "LAB_ComputedDueDate";
+            this.LAB_ComputedDueDate.Size = new System.Drawing.Size(103, 18);
+            this.LAB_ComputedDueDate.TabIndex = 4;
+            this.LAB_ComputedDueDate.Tag = "ComputedDueDate";
+            this.LAB_ComputedDueDate.Text = "ComputedDueDate";
+            this.LAB_ComputedDueDate.Visible = false;
+            // 
+            // LAB_OrgDueDate
+            // 
+            this.LAB_OrgDueDate.Location = new System.Drawing.Point(20, 514);
+            this.LAB_OrgDueDate.Name = "LAB_OrgDueDate";
+            this.LAB_OrgDueDate.Size = new System.Drawing.Size(69, 18);
+            this.LAB_OrgDueDate.TabIndex = 3;
+            this.LAB_OrgDueDate.Tag = "OrgDueDate";
+            this.LAB_OrgDueDate.Text = "OrgDueDate";
+            this.LAB_OrgDueDate.Visible = false;
+            // 
+            // TXT_Name
+            // 
+            this.TXT_Name.Location = new System.Drawing.Point(125, 78);
+            this.TXT_Name.Modified = true;
+            this.TXT_Name.Name = "TXT_Name";
+            this.TXT_Name.Size = new System.Drawing.Size(110, 20);
+            this.TXT_Name.TabIndex = 18;
+            this.TXT_Name.Tag = "Name";
+            // 
+            // LAB_DueDate
+            // 
+            this.LAB_DueDate.Location = new System.Drawing.Point(20, 566);
+            this.LAB_DueDate.Name = "LAB_DueDate";
+            this.LAB_DueDate.Size = new System.Drawing.Size(50, 18);
+            this.LAB_DueDate.TabIndex = 2;
+            this.LAB_DueDate.Tag = "DueDate";
+            this.LAB_DueDate.Text = "DueDate";
+            this.LAB_DueDate.Visible = false;
+            // 
+            // TXT_ID
+            // 
+            this.TXT_ID.Location = new System.Drawing.Point(125, 52);
+            this.TXT_ID.Modified = true;
+            this.TXT_ID.Name = "TXT_ID";
+            this.TXT_ID.Size = new System.Drawing.Size(110, 20);
+            this.TXT_ID.TabIndex = 17;
+            this.TXT_ID.Tag = "ID";
+            // 
+            // LAB_Name
+            // 
+            this.LAB_Name.Location = new System.Drawing.Point(39, 79);
+            this.LAB_Name.Name = "LAB_Name";
+            this.LAB_Name.Size = new System.Drawing.Size(36, 18);
+            this.LAB_Name.TabIndex = 4;
+            this.LAB_Name.Tag = "Name";
+            this.LAB_Name.Text = "Name";
+            // 
+            // StudentEditCtr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.RGB_AboutMateria);
+            this.Name = "StudentEditCtr";
+            this.Size = new System.Drawing.Size(360, 314);
+            this.Controls.SetChildIndex(this.BTN_OK, 0);
+            this.Controls.SetChildIndex(this.BTN_Cancel, 0);
+            this.Controls.SetChildIndex(this.RGB_AboutMateria, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.BTN_OK)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BTN_Cancel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RGB_AboutMateria)).EndInit();
+            this.RGB_AboutMateria.ResumeLayout(false);
+            this.RGB_AboutMateria.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DTP_ReleaseDate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DTP_DueDate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_ReleaseDate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_ComputedReleaseDate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_ID)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_ComputedDueDate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_OrgDueDate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TXT_Name)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_DueDate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TXT_ID)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LAB_Name)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadGroupBox RGB_AboutMateria;
+        private Telerik.WinControls.UI.RadDateTimePicker DTP_ReleaseDate;
+        private Telerik.WinControls.UI.RadDateTimePicker DTP_DueDate;
+        private Telerik.WinControls.UI.RadLabel LAB_ReleaseDate;
+        private Telerik.WinControls.UI.RadLabel LAB_ComputedReleaseDate;
+        private Telerik.WinControls.UI.RadLabel LAB_ID;
+        private Telerik.WinControls.UI.RadLabel LAB_ComputedDueDate;
+        private Telerik.WinControls.UI.RadLabel LAB_OrgDueDate;
+        private Telerik.WinControls.UI.RadTextBox TXT_Name;
+        private Telerik.WinControls.UI.RadLabel LAB_DueDate;
+        private Telerik.WinControls.UI.RadTextBox TXT_ID;
+        private Telerik.WinControls.UI.RadLabel LAB_Name;
+    }
+}
