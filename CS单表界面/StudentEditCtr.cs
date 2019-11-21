f627b216e5b44effb56b635094ece432e8449166
AustinYANyh <1241528539@qq.com>
2019-11-21 21:25:13 +0800

    主界面弹出的界面代码

---
+++ CS单表界面/StudentEditCtr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ORAPS.GUILib;
+
+namespace ORAPS.Fabric
+{
+    public partial class StudentEditCtr : OAS_ObjectEditorCtr
+    { 
+        public StudentEditCtr()
+        {
+            InitializeComponent();
+        }
+    }
+}
