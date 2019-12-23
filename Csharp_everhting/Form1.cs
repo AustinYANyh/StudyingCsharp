2425d7b7fbf462206d514fe0a5a7c6f26c39d342
AustinYANyh <1241528539@qq.com>
2019-12-23 13:07:15 +0800

    扫描输入路径的文本框加入快捷键

--- Csharp_everhting/Form1.cs
+++ Csharp_everhting/Form1.cs
@@ -147,27 +147,114 @@ namespace Csharp_myeverything
             {
                 string[] Next = Directory.GetDirectories(str); //获取当前路径下的所有文件夹
                 string[] Files = Directory.GetFiles(str, FileName);
+                
+
+                HashSet<string> setLocal = new HashSet<string>();
+                HashSet<string> setDb = new HashSet<string>();
+                List<string> res = Data.selectSql(str);
+
+                for (int a = 0; a < Next.Count(); ++a)
+                {
+                    Next[a] = Next[a].Replace('\\', '/');
+                    setLocal.Add(Next[a].Substring(Next[a].LastIndexOf('/') + 1));
+                    FilePath.Add(Next[a]);
+                }
+                for (int a = 0; a < Files.Count(); ++a)
+                {
+                    Files[a] = Files[a].Replace('\\', '/');
+                    setLocal.Add(Files[a].Substring(Files[a].LastIndexOf('/') + 1));
+                }
+                foreach(string e in res)
+                {
+                    setDb.Add(e);
+                }
+
+                //int size = Math.Min(setLocal.Count, setDb.Count);
 
                 string NAME = "";
                 string PATH = "";
                 string temp = "";
 
-                foreach (string Nowpath in Next)
-                {    
-                    temp = Nowpath;
-                    PATH = temp.Substring(0, temp.LastIndexOf('\\'));
-                    NAME = temp.Substring(temp.LastIndexOf('\\') + 1);
-                    Data.insertSql(PATH, NAME);
-                    FilePath.Add(Nowpath);
+                int i=0;int j=0;
+
+                if (setDb.Count != 0 && setLocal.Count != 0)
+                {
+                    while (i < setLocal.Count && j < setDb.Count)
+                    {
+                        //本地有,数据库没有,插入到数据库
+                        if (String.Compare(setLocal.ElementAt(i), setDb.ElementAt(j)) == -1)
+                        {
+                            foreach (string Nowpath in Next)
+                            {
+                                temp = Nowpath;
+                                PATH = temp.Substring(0, temp.LastIndexOf('/'));
+                                NAME = temp.Substring(temp.LastIndexOf('/') + 1);
+                                Data.insertSql(PATH, NAME);
+                                FilePath.Add(Nowpath);
+                            }
+
+                            foreach (string file in Files)
+                            {
+                                temp = file;
+                                PATH = temp.Substring(0, temp.LastIndexOf('/'));
+                                NAME = temp.Substring(temp.LastIndexOf('/') + 1);
+                                Data.insertSql(PATH, NAME);
+                                FilePath.Add(file);
+                            }
+                            i++;
+                        }
+                        //从数据库删除
+                        else if (String.Compare(setLocal.ElementAt(i), setDb.ElementAt(j)) == 1)
+                        {
+                            Data.deleteSql(setDb.ElementAt(j).ToString());
+                            j++;
+                        }
+                        else
+                        {
+                            i++;
+                            j++;
+                        }
+                    }
                 }
 
-                foreach (string file in Files)
+                if (setLocal.Count != 0)
                 {
-                    temp = file;
-                    PATH = temp.Substring(0, temp.LastIndexOf('\\'));
-                    NAME = temp.Substring(temp.LastIndexOf('\\') + 1);
-                    Data.insertSql(PATH, NAME);
-                    FilePath.Add(file);
+                    //while (setLocal.ElementAt(i) != setLocal.Last())
+                    while (i < setLocal.Count)
+                    {
+                        foreach (string Nowpath in Next)
+                        {
+                            temp = Nowpath;
+                            PATH = temp.Substring(0, temp.LastIndexOf('/'));
+                            NAME = temp.Substring(temp.LastIndexOf('/') + 1);
+                            
+                                Data.insertSql(PATH, NAME);
+                                i++;
+                                //i = Math.Min(i, setLocal.Count - 1);
+                            
+                            //FilePath.Add(Nowpath);
+                        }
+
+                        foreach (string file in Files)
+                        {
+                            temp = file;
+                            PATH = temp.Substring(0, temp.LastIndexOf('/'));
+                            NAME = temp.Substring(temp.LastIndexOf('/') + 1);
+                            Data.insertSql(PATH, NAME);
+                            i++;
+                            //i = Math.Min(i, setLocal.Count - 1);
+                            FilePath.Add(file);
+                        }
+                    }
+                }
+
+                if (setDb.Count != 0)
+                {
+                    while (j < setDb.Count)
+                    {
+                        Data.deleteSql(setDb.ElementAt(j).ToString());
+                        j++;
+                    }
                 }
 
                 GetFolderAndFile(Next, FilePath, FileName);
@@ -202,6 +289,55 @@ namespace Csharp_myeverything
             {
                 MessageBox.Show("插入数据库失败!");
             }
+            mysql.Close();
+        }
+
+        static public List<string> selectSql(string path)
+        {
+            //链接数据库
+            MySqlConnection mysql = new MySqlConnection
+                ("server=111.229.13.33;User Id=luzihan;password=124152;Database=myeverything");
+
+            string sql = string.Format("select * from everything where path = '{0}';", path);
+
+            //打开数据库执行sql语句
+            mysql.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, mysql);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            List<string> res = new List<string>();
+
+            if(dataTable.Rows.Count > 0)
+            {
+                foreach (DataRow dr in dataTable.Rows)
+                {
+                    res.Add(dr["name"].ToString());
+                }
+            }
+
+            mysql.Close();
+            return res;
+        }
+
+        static public void deleteSql(string name)
+        {
+            //链接数据库
+            MySqlConnection mysql = new MySqlConnection
+                ("server=111.229.13.33;User Id=luzihan;password=124152;Database=myeverything");
+
+            string sql = string.Format("select * from everything where name = '{0}';", name);
+
+            //打开数据库执行sql语句
+            mysql.Open();
+            MySqlCommand cmd = new MySqlCommand(sql, mysql);
+            if (cmd.ExecuteNonQuery() < 0)
+            {
+                MessageBox.Show("数据库删除失败!");
+            }
+
             mysql.Close();
         }
     }
