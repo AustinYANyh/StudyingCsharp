6125df699ecadc832602799c10d386557c1e1e5c
AustinYANyh <1241528539@qq.com>
2020-01-08 18:39:26 +0800

    加入注册功能

--- C#学习/login.cs
+++ C#学习/login.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace chatroom
 {
     public partial class login : Form
     {
+        private static MySqlConnection mysql = new MySqlConnection
+                ("server=111.229.13.33;User Id=luzihan;password=124152;Database=chat_login");
         public login()
         {
             InitializeComponent();
@@ -22,19 +25,69 @@ namespace chatroom
             switch(e.KeyCode)
             {
                 case Keys.Enter:
-                    if (user.username == EDI_USERNAME.Text && user.passwd == EDI_PASSWD.Text)
-                    {
-                        this.DialogResult = DialogResult.OK;
-                    }
+                    Func_login();
                     break;
             }
         }
+
+        private void Func_login()
+        {
+            if (checkLogin(EDI_USERNAME.Text, EDI_PASSWD.Text) == true)
+            {
+                user.username = EDI_USERNAME.Text;
+                this.DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                MessageBox.Show("用户名或密码错误，请重试！");
+                EDI_PASSWD.Clear();
+            }
+        }
+
+        private bool checkLogin(string name,string passwd)
+        {
+            mysql.Open();
+            string sql = "select * from loginfo where log_name = '";
+            sql += name;
+            sql += "';";
+
+            MySqlCommand cmd = new MySqlCommand(sql, mysql);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable datatable = new DataTable();
+            datatable.Clear();
+            adapter.Fill(datatable);
+
+            if (datatable.Rows.Count == 0)
+            {
+                mysql.Close();
+                return false;
+            }
+            else
+            {
+                if(passwd == datatable.Rows[0]["log_passwd"].ToString())
+                {
+                    mysql.Close();
+                    return true;
+                }
+            }
+            mysql.Close();
+            return false;
+        }
+
+        private void BTN_LOGIN_Click(object sender, EventArgs e)
+        {
+            Func_login();
+        }
+
+        private void BTN_LOGON_Click(object sender, EventArgs e)
+        {
+            logon log_on = new logon();
+            log_on.ShowDialog();
+        }
     }
 
-    //懒,所以用户名和密码写死
     public partial class user : Form
     {
-        public static string username = "luzihan";   //注意全局变量要使用static 
-        public static string passwd = "zhu";
+        public static string username = "";   //注意全局变量要使用static 
     }
 }
---
+++ C#学习/logon.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace chatroom
+{
+    public partial class logon : Form
+    {
+        private static MySqlConnection mysql = new MySqlConnection
+                ("server=111.229.13.33;User Id=luzihan;password=124152;Database=chat_login");
+        public logon()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_NO_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BTN_YES_Click(object sender, EventArgs e)
+        {
+            string name = EDI_LOGON_NAME.Text.Trim();
+            string passwd = EDI_LOGON_PASSWD.Text.Trim();
+
+            if(name.Length <= 0 || passwd.Length <= 0)
+            {
+                MessageBox.Show("用户名或密码不合法,请重试...");
+            }
+
+            mysql.Open();
+
+            string sql = "";
+            sql = string.Format("insert into loginfo values('{0}','{1}');", name, passwd);
+
+            MySqlCommand cmd = new MySqlCommand(sql, mysql);
+            if (cmd.ExecuteNonQuery() < 0)
+            {
+                MessageBox.Show("插入数据库失败!");
+            }
+            
+            mysql.Close();
+            this.Close();
+        }
+    }
+}
