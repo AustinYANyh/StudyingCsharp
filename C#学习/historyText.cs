56d77a2e32764733999a1caa2fda4838952ef8f7
AustinYANyh <1241528539@qq.com>
2020-02-10 14:21:21 +0800

    聊天记录模拟实现的代码

---
+++ C#学习/historyText.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public BinaryWriter bw;
+        public BinaryReader br;
+
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.LoadFile("./history.txt", RichTextBoxStreamType.PlainText);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+            richTextBox1.AppendText(DateTime.Now.ToString()+"\r\n");
+            richTextBox1.AppendText(richTextBox2.Text+"\r\n");
+            richTextBox2.Clear();
+
+            /*
+            bw = new BinaryWriter(new FileStream("./history.txt", FileMode.Append));
+            bw.Write(richTextBox1.Text);
+            bw.Close();
+            */
+
+            richTextBox1.SaveFile("./history.txt", RichTextBoxStreamType.PlainText);
+        }
+    }
+}
