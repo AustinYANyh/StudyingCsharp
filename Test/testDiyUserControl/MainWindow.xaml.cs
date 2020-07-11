f754c31de0112bf20c5a1e413995cafdf180b5cd
AustinYANyh <1241528539@qq.com>
2020-07-11 15:07:35 +0800

    换成ListView实现同样功能

--- Test/testDiyUserControl/MainWindow.xaml.cs
+++ Test/testDiyUserControl/MainWindow.xaml.cs
@@ -21,6 +21,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Documents;
 using System.IO;
+using Point = System.Windows.Point;
+using Brushes = System.Windows.Media.Brushes;
 
 namespace testLineAttritube
 {
@@ -120,70 +122,97 @@ namespace testLineAttritube
             HotKey.RegisterHotKey(wih.Handle, altd, HotKey.KeyModifiers.Alt, (int)Keys.D);
         }
 
+        public string[] str;
         void HighLight()
         {
-            string txt = string.Empty;
-            txt = OrderShow.Text;
-            OrderShow.Focus();
+            //string txt = string.Empty;
+            //txt = OrderShow.Text;
+            //OrderShow.Focus();
 
-            string[] str = txt.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-            int index = 0;
-            for (int i = 0; i < str.Count(); ++i)
-            {
-                OrderShow.Focus();
-                OrderShow.CaretIndex = 0;
-                OrderShow.Select(index, str[i].Length);
-                OrderShow.ScrollToLine(i);
-                Thread t = new Thread(o => Thread.Sleep(200));
-                t.Start(this);
-                while (t.IsAlive)
-                    System.Windows.Forms.Application.DoEvents();
-                index += str[i].Length + 2;
-            }
-            Importbtn.Focus();
-            //TextRange textrange = new TextRange(OrderShow.Document.ContentStart, OrderShow.Document.ContentEnd);
-            //string[] str = textrange.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            //string[] str = txt.Split(new string[] { "\r\n" }, StringSplitOptions.None);
             //int index = 0;
             //for (int i = 0; i < str.Count(); ++i)
             //{
-            //    Dispatcher.BeginInvoke(new Action(() =>
+            //    OrderShow.Focus();
+            //    OrderShow.CaretIndex = 0;
+            //    OrderShow.Select(index, str[i].Length);
+            //    OrderShow.ScrollToLine(i);
+            //    Thread t = new Thread(o => Thread.Sleep(200));
+            //    t.Start(this);
+            //    while (t.IsAlive)
+            //        System.Windows.Forms.Application.DoEvents();
+            //    index += str[i].Length + 2;
+            //}
+            //Importbtn.Focus();
+
+            //TextRange textrange = new TextRange(OrderShow.Document.ContentStart, OrderShow.Document.ContentEnd);
+            //string[] str = textrange.Text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+            //int index = 0;
+            //for (int i = 0; i < str.Count(); ++i)
+            //{
+            //    try
             //    {
             //        textrange = new TextRange(OrderShow.Document.ContentStart, OrderShow.Document.ContentEnd);
             //        textrange.ApplyPropertyValue(TextElement.ForegroundProperty, System.Windows.Media.Brushes.Black);
             //        textrange.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Regular);
-            //    }));
-            //    TextPointer p1 = OrderShow.Selection.Start;
-            //    p1 = p1.GetPositionAtOffset(index);
-            //    index += str[i].Length;
-            //    TextPointer p2 = OrderShow.Selection.Start;
-            //    p2 = p2.GetPositionAtOffset(index);
-            //    Dispatcher.BeginInvoke(new Action(() =>
-            //    {
+
+            //        TextPointer p1 = OrderShow.Selection.Start;
+            //        p1 = p1.GetPositionAtOffset(index);
+            //        index += str[i].Length;
+            //        TextPointer p2 = OrderShow.Selection.Start;
+            //        p2 = p2.GetPositionAtOffset(index);
+
             //        textrange = new TextRange(p1, p2);
             //        textrange.ApplyPropertyValue(TextElement.ForegroundProperty, System.Windows.Media.Brushes.Blue);
             //        textrange.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Bold);
-            //    }));
-            //    index += 4;
-            //    Thread.Sleep(1000);
+            //        index += 4;
+            //        Thread t = new Thread(o => Thread.Sleep(200));
+            //        t.Start(this);
+            //        while (t.IsAlive)
+            //            System.Windows.Forms.Application.DoEvents();
+            //        decimal bb = (decimal)(3178 / (str.Count() + 1));
+            //        double ii = (double)((decimal)(3178 / (str.Count() + 1)) * (i + 1));
+            //        OrderShow.ScrollToVerticalOffset(ii);
+            //        DependencyObject currObj = OrderShow.CaretPosition.Parent;
+            //        FrameworkContentElement fce = currObj as FrameworkContentElement;
+            //        if (fce != null)
+            //        {
+            //            int a = (i) * (-1) * (p2.GetOffsetToPosition(OrderShow.Document.ContentStart));
+            //        }
+            //    }
+            //    catch { }
             //}
-            //Dispatcher.BeginInvoke(new Action(() =>
-            //{
-            //    textrange = new TextRange(OrderShow.Document.ContentStart, OrderShow.Document.ContentEnd);
-            //    textrange.ApplyPropertyValue(TextElement.ForegroundProperty, System.Windows.Media.Brushes.Black);
-            //    textrange.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Regular);
-            //}));
+            //textrange = new TextRange(OrderShow.Document.ContentStart, OrderShow.Document.ContentEnd);
+            //textrange.ApplyPropertyValue(TextElement.ForegroundProperty, System.Windows.Media.Brushes.Black);
+            //textrange.ApplyPropertyValue(TextElement.FontWeightProperty, FontWeights.Regular);
+
+
+            for (int i = 0; i < str.Count(); ++i)
+            {
+                listview.ScrollIntoView(listview.Items[i]);
+                (listview.Items[i] as TextBlock).Foreground = Brushes.Red;
+                (listview.Items[i] as TextBlock).Background = Brushes.Yellow;
+                (listview.Items[i] as TextBlock).FontWeight = FontWeights.Bold;
+                (listview.Items[i] as TextBlock).FontSize = 16;
+                Thread t = new Thread(o => Thread.Sleep(100));
+                t.Start(this);
+                while (t.IsAlive)
+                    System.Windows.Forms.Application.DoEvents();
+                (listview.Items[i] as TextBlock).Foreground = Brushes.White;
+                (listview.Items[i] as TextBlock).Background = Brushes.Transparent;
+                (listview.Items[i] as TextBlock).FontWeight = FontWeights.Regular;
+                (listview.Items[i] as TextBlock).FontSize = 13;
+            }
+            listview.ScrollIntoView(listview.Items[0]);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            OrderShow.CaretIndex = 0;
-            OrderShow.IsReadOnly = true;
+            //OrderShow.CaretIndex = 0;
+            //OrderShow.IsReadOnly = true;
 
-            OrderShow.SelectionBrush = System.Windows.Media.Brushes.Aqua;
-                //Thread thread = new Thread(HighLight);
+            //OrderShow.SelectionBrush = System.Windows.Media.Brushes.Aqua;
             HighLight();
-            //thread.IsBackground = true;
-            //thread.Start();
         }
 
         private void Importbtn_Click(object sender, RoutedEventArgs e)
@@ -191,10 +220,42 @@ namespace testLineAttritube
             using (StreamReader sr = new StreamReader(@"C:\Users\29572\Desktop\a - 副本 (2) - 副本.nc", Encoding.Default))
             {
                 string txt = sr.ReadToEnd();
-                OrderShow.Clear();
-                OrderShow.Text = txt;
-                sr.Close();
+                listview.Items.Clear();
+                //OrderShow.Clear();
+                //OrderShow.Text = txt;
+                //OrderShow.Document.Blocks.Clear();
+                //OrderShow.AppendText(txt);
+                str = txt.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                for (int i = 0; i < str.Count(); ++i)
+                {
+                    TextBlock block = new TextBlock();
+                    block.FontSize = 13;
+                    block.Foreground = Brushes.White;
+                    block.Text = str[i];
+                    listview.Items.Add(block);
+                    sr.Close();
+                }
             }
+        }
+
+        private void listview_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void listview_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void listview_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void listview_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            e.Handled = true;
         }
 
         private IntPtr MainWindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
