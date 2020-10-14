689be038fb0fd00cf2c15789cc83f4b54db95a9e
AustinYANyh <1241528539@qq.com>
2020-10-14 16:57:34 +0800

    学习Blend制作WPF的动画效果（三）（窗体启动、关闭动画）

--- Test/testAnimation.xaml.cs
+++ Test/testAnimation.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,12 +22,23 @@ namespace testAnimation
     /// </summary>
     public partial class MainWindow : Window
     {
+        Storyboard closeStoryboard;
         public MainWindow()
         {
             InitializeComponent();
-
-            //Storyboard storyboard = (Storyboard)this.Resources["Storyboard1"];
-            //storyboard.Begin();
+            closeStoryboard = (Storyboard)this.FindResource("closeStoryboard");
+            closeStoryboard.Completed += (c, d) => Application.Current.Shutdown(); ;
         }
-    }
-}
+
+        private void window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            closeStoryboard.Begin();
+            e.Cancel = true;
+        }
+
+        private void row1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }// end for class
+}// end for namespace
