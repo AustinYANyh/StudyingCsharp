954ec18764ae28543e2b4f981cb5a98d4f0ca138
AustinYANyh <1241528539@qq.com>
2020-11-28 16:27:28 +0800

    界面的数据绑定及单位绑定

---
+++ Test/testUnit/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System;
+
+namespace testUnit
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new MainWindowViewModel();
+            this.DataContext = viewModel;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UnitCombox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //转换前准备工作
+            HMIUnitConvertLib.SpeedUnitHelper.speedUnitMode = UnitCombox.SelectedIndex;
+
+            //速度单位换算
+            viewModel.Speed1 = HMIUnitConvertLib.SpeedUnitHelper.HMISpeedUnitConvert(viewModel.Speed1);
+            viewModel.Speed2 = HMIUnitConvertLib.SpeedUnitHelper.HMISpeedUnitConvert(viewModel.Speed2);
+            viewModel.Speed3 = HMIUnitConvertLib.SpeedUnitHelper.HMISpeedUnitConvert(viewModel.Speed3);
+            viewModel.Speed4 = HMIUnitConvertLib.SpeedUnitHelper.HMISpeedUnitConvert(viewModel.Speed4);
+
+            //最后才可以更新上一次速度选择的索引
+            if (UnitCombox.SelectedIndex == -1)
+                HMIUnitConvertLib.SpeedUnitHelper.speedPreindex = 0;
+            else
+                HMIUnitConvertLib.SpeedUnitHelper.speedPreindex = UnitCombox.SelectedIndex;
+        }
+    }
+}
---
+++ Test/testUnit/MainWindowViewModel.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace testUnit
+{
+    class MainWindowViewModel : NotificationObject
+    {
+        public MainWindowViewModel()
+        {
+            Task task = new Task(() =>
+            {
+                RefrushData();
+            });
+            task.Start();
+        }
+        List<string> UnitList = new List<string>() { "mm/s", "mm/min", "m/s", "m/min", "in/s", "in/min" };
+        private string _SpeedUnit;
+        public string SpeedUnit
+        {
+            get { return _SpeedUnit; }
+            set
+            {
+                _SpeedUnit = value;
+                RaisePropertyChanged("SpeedUnit");
+            }
+        }
+
+        private int _PreSpeedMode = 0;
+        public int PreSpeedMode
+        {
+            get { return _PreSpeedMode; }
+            set
+            {
+                _PreSpeedMode = value;
+                RaisePropertyChanged("PreSpeedMode");
+            }
+        }
+
+        private int _CurSpeedMode = 0;
+        public int CurSpeedMode
+        {
+            get { return _CurSpeedMode; }
+            set
+            {
+                _CurSpeedMode = value;
+                RaisePropertyChanged("CurSpeedMode");
+            }
+        }
+
+        private double _Speed1 = 100;
+        public double Speed1
+        {
+            get { return _Speed1; }
+            set
+            {
+                _Speed1 = value;
+                RaisePropertyChanged("Speed1");
+            }
+        }
+
+        private double _Speed2 = 200;
+        public double Speed2
+        {
+            get { return _Speed2; }
+            set
+            {
+                _Speed2 = value;
+                RaisePropertyChanged("Speed2");
+            }
+        }
+
+        private double _Speed3 = 300;
+        public double Speed3
+        {
+            get { return _Speed3; }
+            set
+            {
+                _Speed3 = value;
+                RaisePropertyChanged("Speed3");
+            }
+        }
+
+        private double _Speed4 = 50;
+        public double Speed4
+        {
+            get { return _Speed4; }
+            set
+            {
+                _Speed4 = value;
+                RaisePropertyChanged("Speed4");
+            }
+        }
+
+        public void RefrushData()
+        {
+            while (true)
+            {
+                SpeedUnit = UnitList[CurSpeedMode];
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
