ecad3a0a0e0cf8a34043ad3ab63f2edd46738f3f
AustinYANyh <1241528539@qq.com>
2020-12-02 14:24:46 +0800

    添加IOC容器，便于管理Model数据

---
+++ Test/testUnit/Helper/SimpleIOC.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testUnit
+{
+    public static class SimpleIOC
+    {
+        public static UnityContainer container = new UnityContainer();
+        public static UnityServiceLocator simpleIoc = new UnityServiceLocator(container);
+    }
+}
