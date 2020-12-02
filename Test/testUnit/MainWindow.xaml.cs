b6270b0f2b7c9e60ce90bffb1709b4b348e45a6c
AustinYANyh <1241528539@qq.com>
2020-12-02 16:09:16 +0800

    xaml界面布局

---
+++ Test/testUnit/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.IO;
+
+namespace testUnit
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new MainWindowViewModel();
+            this.DataContext = viewModel;
+        }
+    }
+}
