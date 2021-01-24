9abe0efc2b170f745d31ae030b7bddd7a2f6c2cd
LuzihanYYH <2823568349@qq.com>
2021-01-24 20:03:37 +0800

    dll加载异常处理的方法

---
+++ Test/InstallPackage/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Windows;
+
+namespace Installationpackage
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += this.CurrentDomain_AssemblyResolve;
+
+            base.OnStartup(e);
+        }
+
+        //找不到dll时, 从自身嵌入的资源中加载
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string text = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
+            text = text.Replace(".", "_");
+            Assembly result;
+            if (text.EndsWith("_resources"))
+            {
+                return null;
+            }
+            else
+            {
+                ResourceManager resourceManager = new ResourceManager(base.GetType().Namespace + ".Properties.Resources", Assembly.GetExecutingAssembly());
+                byte[] rawAssembly = (byte[])resourceManager.GetObject(text);
+                result = Assembly.Load(rawAssembly);
+            }
+            return result;
+        }
+
+        //全局异常处理
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+        }
+    }
+}
