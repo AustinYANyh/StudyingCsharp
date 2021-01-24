54098cdcf331b032b153d491e8481aff999e4bde
LuzihanYYH <2823568349@qq.com>
2021-01-24 20:00:36 +0800

    全局IOC容器和资源提取的方法

---
+++ Test/InstallPackage/Helper/IocHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Practices.Unity;
+
+namespace Installationpackage.Helper
+{
+    public class IocHelper
+    {
+        public static UnityContainer container = new UnityContainer();
+
+        public static UnityServiceLocator SimpleIOC = new UnityServiceLocator(IocHelper.container);
+    }
+}
---
+++ Test/InstallPackage/Helper/ResourcesHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Installationpackage.Helper
+{
+    public class ResourcesHelper
+    {
+        public static void ExtractResFile(string resFileName, string outputFile)
+        {
+            BufferedStream bufferedStream = null;
+            FileStream fileStream = null;
+            try
+            {
+                Assembly executingAssembly = Assembly.GetExecutingAssembly();
+                bufferedStream = new BufferedStream(executingAssembly.GetManifestResourceStream(resFileName));
+                fileStream = new FileStream(outputFile, FileMode.Create, FileAccess.Write);
+                byte[] array = new byte[1024];
+                int count;
+                while ((count = bufferedStream.Read(array, 0, array.Length)) > 0)
+                {
+                    fileStream.Write(array, 0, count);
+                }
+                fileStream.Flush();
+            }
+            finally
+            {
+                if (fileStream != null)
+                {
+                    fileStream.Close();
+                }
+                if (bufferedStream != null)
+                {
+                    bufferedStream.Close();
+                }
+            }
+        }
+    }
+}
